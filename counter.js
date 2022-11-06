9e467e295ca40020a54dd01b8d211159c47f836c
SAYYED MOHD ARIF <112590931+SAYYEDMOHDARIF@users.noreply.github.com>
2022-11-07 03:17:03 +0530

    RANDOMNUMBERGEBRATOR
    RANDOMNUMBERGEBRATOR

---
+++ counter.js
@@ -0,0 +1,13 @@
+
+document.getElementById('btn3')
+btn3.addEventListener('click',myfunction)
+//document.getElementById('btn2')
+//btn2.addEventListener('click',myyfunction)
+//btn1.addEventListener('click',myyyfunction)
+function myfunction()
+{
+     
+     document.getElementById("demo").innerHTML =Math.floor(Math.random()*101);
+     
+}
+
